adb70e4330a24261453ffd79a1f780a15ac07dd9
naman159 <namanranawat159@gmail.com>
2020-05-15 21:55:10 +0530

    Posts UI

--- comments/index.js
+++ comments/index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const bParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
-const { randomBytes } = require('crypto');
+const express = require("express");
+const bParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+const { randomBytes } = require("crypto");
 
 const app = express();
+app.use(cors());
 app.use(bParser.json());
 
 const comments = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
   res.send(comments[req.params.id] || []);
-;})
+});
 
-app.post('/posts/:id/comments', (req, res) => {
-  const id = randomBytes(4).toString('hex');
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const commentsById = comments[req.params.id] || [];
 
-  commentsById.push({id, content});
+  commentsById.push({ id, content });
 
   comments[req.params.id] = commentsById;
 
   res.status(201).send(comments);
-
-})
+});
 
 app.listen(7000, () => {
   console.log(`Posts online on PORT 7000`);
-})
+});
